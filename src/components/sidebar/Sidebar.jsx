f6cac934ebd1f618d7c8669371aac29d874834db
Emad Saeed <md.adv1@gmail.com>
2022-07-13 21:40:21 +0100

    stage 1

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -9,7 +9,7 @@ import ContactMailIcon from '@mui/icons-material/ContactMail';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
-test test
+
 function Sidebar() {
     return (
         <div className="sidebar" >
